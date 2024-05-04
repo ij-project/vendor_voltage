d26f458c3fecabf896294c8b8d6e1537ede9506c
Bruno Martins <bgcngm@gmail.com>
2024-05-04 04:14:43 +0000

    Revert "Hookup SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC"
    This reverts commit bc8d773.
    
    Reason for revert: no longer needed when using blobs that match tag
    DISPLAY.LA.3.0.r1-10700-KAILUA.0.
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: Ia89ce566a4cade6091a2dc6bc4e8b56ef7d1f73b

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -38,7 +38,6 @@ SOONG_CONFIG_voltageGlobalVars += \
     gralloc_handle_has_custom_content_md_reserved_size \
     needs_netd_direct_connect_rule \
     target_alternative_futex_waiters \
-    sdmcore_has_is_display_hw_available_func \
     target_health_charging_control_charging_path \
     target_health_charging_control_charging_enabled \
     target_health_charging_control_charging_disabled \
@@ -83,7 +82,6 @@ SOONG_CONFIG_voltageQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBR
 SOONG_CONFIG_voltageQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_voltageQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_voltageGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
-SOONG_CONFIG_voltageGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
@@ -99,7 +97,6 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE ?= false
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE ?= true
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_QTI_VIBRATOR_EFFECT_LIB ?= libqtivibratoreffect
-TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC ?= true
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 TARGET_TRUST_USB_CONTROL_PATH ?= /proc/sys/kernel/deny_new_usb
 TARGET_TRUST_USB_CONTROL_ENABLE ?= 1
