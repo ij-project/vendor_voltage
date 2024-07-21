ea023ad3f511a260b308d19a2bb570dcdf473a9c
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 21:04:37 +0200

    config: tablet: Deduplicate full tablet configuration
    Change-Id: I12cc09d4fca28d063cea76301de287526bfec570
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -1,10 +1,7 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
-
-# Inherit mobile full common VoltageOS stuff
+# Inherit mobile full common Voltage stuff
 $(call inherit-product, vendor/voltage/config/common_mobile_full.mk)
 
-# Settings
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.settings.large_screen_opt.enabled=true
+# Inherit full tablet common Voltage stuff
+$(call inherit-product, vendor/voltage/config/full_tablet.mk)
 
 $(call inherit-product, vendor/voltage/config/telephony.mk)
