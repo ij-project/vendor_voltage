632232e302ef36c2210922ebe2729b094dec4765
Kanishk <kanishkthederp@gmail.com>
2024-08-07 17:12:17 +0530

    vendor: release 3.6
    Signed-off-by: Kanishk <kanishkthederp@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 ANDROID_VERSION := 14
-VOLTAGEVERSION := 3.5
+VOLTAGEVERSION := 3.6
 
 VOLTAGE_BUILD_TYPE ?= UNOFFICIAL
 VOLTAGE_DATE_YEAR := $(shell date -u +%Y)
