259e1014fbdad536137c2fc0071a33f5843197ed
Dmitrii <bankersenator@gmail.com>
2024-09-05 02:48:26 +0000

    vendor: 4.0
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ANDROID_VERSION := 14
-VOLTAGEVERSION := 3.7-EOL
+ANDROID_VERSION := 15
+VOLTAGEVERSION := 4.0
 
 VOLTAGE_BUILD_TYPE ?= UNOFFICIAL
 VOLTAGE_DATE_YEAR := $(shell date -u +%Y)
