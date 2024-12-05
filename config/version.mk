caac5b0b0c516c7e7421d3dac909eef1a1627112
Kanishk <kanishkthederp@gmail.com>
2024-12-05 23:52:23 +0530

    vendor: Release 4.0.1
    Signed-off-by: Kanishk <kanishkthederp@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 ANDROID_VERSION := 15
-VOLTAGEVERSION := 4.0
+VOLTAGEVERSION := 4.0.1
 
 VOLTAGE_BUILD_TYPE ?= UNOFFICIAL
 VOLTAGE_DATE_YEAR := $(shell date -u +%Y)
